fbd5ddc25468879ab0655a7a0310f5a653e5a7aa
tortvana123 <34056674+tortvana123@users.noreply.github.com>
2017-12-08 12:33:52 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-#-------------------------------------------------------------------------------------------------------------------------------------------
-
+import appJar # learn more: https://python.org/pypi/appJar
+#-----------------------------------------------------------------------------------------------------------------------------------------
 # Imports
+
 from ruamel import yaml
+from appJar import gui
 
-#-------------------------------------------------------------------------------------------------------------------------------------------
-
+#-----------------------------------------------------------------------------------------------------------------------------------------
 #YAML input/output, compatible with old and new versions of ruamel.yaml
 def yml_io(ser_type, istream=None): # ser_type - "serialize" data into YAML or "deserialize" data from YAML; istream - input data  
     
@@ -26,6 +27,33 @@ def yml_io(ser_type, istream=None): # ser_type - "serialize" data into YAML or "
             yml.dump(istream, ostream)
             return ostream
 
-#-------------------------------------------------------------------------------------------------------------------------------------------
-with open("info.yaml") as f:
-    print(yml_io("deserialize", f))
+def init_data():
+    pass
+def title_screen():
+    pass
+
+def main():
+    init_data()
+    title_screen()
+
+
+main()
+#-----------------------------------------------------------------------------------------------------------------------------------------
+# GUI TESTING
+def press(btn):
+    print(btn)
+
+
+with gui("TESTING GAME", "400x200") as app:
+    app.addLabel("Game_Title", "WELCOME!")
+    app.addMessage("msg1", """You can put a lot of text in this widget.
+The text will be wrapped over multiple lines.
+It's not possible to apply different styles to different words.""")
+    app.addButton("One", press)
+    app.addButton("Two", press)
+    app.addButton("Three", press)
+    app.go()
+#-----------------------------------------------------------------------------------------------------------------------------------------
+# TEST CODE
+#with open("info.yaml") as f:
+    #print(yml_io("deserialize", f))
