42a121404408f246328cdd84c268e6d0edbc65d9
Unknown <tortvana123@gmail.com>
2018-01-04 22:23:09 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 # Imports
-from ruamel import yaml
+# -----------------------------------------------------------------------
+import ruamel.yaml as yaml
 from appJar import gui
 from random import choice
-#-----------------------------------------------------------------------
-# Variables
-conf_file = "info.yaml"
-#-----------------------------------------------------------------------
-#Functions and classes
 
-#YAML input/output, compatible with old and new versions of ruamel.yaml
-def yml_io(ser_type, istream=None): # ser_type - "serialize" data into YAML or "deserialize" data from YAML; istream - input data  
-	
+# -----------------------------------------------------------------------
+# Variables
+# -----------------------------------------------------------------------
+conf_file = "info.yaml"
+# -----------------------------------------------------------------------
+# Functions and classes
+# -----------------------------------------------------------------------
+
+# ----
+# YAML input/output, compatible with old and new versions of ruamel.yaml
+def yml_io(ser_type,istream=None):  # ser_type - "serialize" data into YAML or "deserialize" data from YAML; istream - input data
     ostream = ""
-    
+
     if yaml.version_info < (0, 15):
         if ser_type == "deserialize":
             ostream = yaml.safe_load(istream)
@@ -30,44 +34,57 @@ def yml_io(ser_type, istream=None): # ser_type - "serialize" data into YAML or "
             yml.dump(istream, ostream)
             return ostream
 
+
+# ----
 # Intialize data from the data file.
-def init_data(): 
+def init_data():
     global data
     with open(conf_file) as yaml_data:
         data = yml_io("deserialize", yaml_data)
 
-#Button function
+
+# ----
+# Button function
 def press(btn):
     if btn == "Start Game":
         game()
 
-#Room class (WIP)
-"""class Room:
+# ----
+# Room class (WIP)
+class Room:
 
-    def __init__(self, name): #Data init
+    def __init__(self, name):  # Data init
         self.name = name
 
     def enter_room(self):
         print("")
-"""
-        
+
+
+# ----
 # GAME (WIP)
 def game():
-    app.setLabel("Title_Label", choice(data["room_names"]))
-    app.setLabel("Message", choice(data["room_descriptions"]))
+    pass
 
-# Title screen  
+    # app.setLabel("Title_Label", choice(data["room_names"]))
+    # app.setLabel("Message", choice(data["room_descriptions"]))
+
+
+# ----
+# Title screen
 def title_screen():
     global app
     with gui(data["title_titlebar"]) as app:
         app.addLabel("Title_Label", data["title_label"])
         app.addLabel("Message", data["title_description"])
         app.addButton("Start Game", press)
-                 
+
+
+# ----
 # Main
 def main():
     init_data()
     title_screen()
 
-if __name__ == "__main__": # If the file is used as a game, not a library
+
+if __name__ == "__main__":  # If the file is used as a game, not a library
     main()
